ae8cc21c1ba663a56d0983171e25d631b05e504f
Jeremy <jerm.tron@gmail.com>
2024-03-02 15:38:16 -0500

    dreated 16 x 16 grid

--- Etch.js
+++ Etch.js
@@ -1,1 +1,11 @@
+const container = document.querySelector("#container");
+/* creating a grid thats 16 x 16 */
 
+function standardGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
+    let divs = document.createElement("div");
+    divs.classList.add("squares");
+    container.appendChild(divs);
+  }
+}
+standardGrid()
